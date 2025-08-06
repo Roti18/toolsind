8de7c45cf17f544c40f2c0a25cb5bd26e895562c
Roti18 <akuronif@gmail.com>
2025-08-06 17:52:17 +0700

    add: data

---
+++ data/toolsCard.tsx
@@ -0,0 +1,182 @@
+import { QrCode } from "lucide-react";
+
+export interface ToolCard {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+  href: string;
+}
+
+const toolsCard = [
+  {
+    href: "/tools/qr-code-generator",
+    title: "QR Code Generator",
+    description: "Buat & scan QR code dengan cepat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+  {
+    href: "readme-ai-generator.vercel.app/",
+    title: "Readme Generator",
+    description: "Buat Readme otomatis dengan AI dan edit langsung ditempat",
+    icon: QrCode,
+  },
+];
+
+export default toolsCard;
